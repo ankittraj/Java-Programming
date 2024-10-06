42abfc1444e34958f544500d4c94809ffb69884f
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2024-10-06 23:04:24 +0530

    Add files via upload

---
+++ findLargest.java
@@ -0,0 +1,12 @@
+public class findLargest {
+    public static void main(String args[]){
+        int A=1, B=3, C=6;
+        if((A>=B) && (A>=C)){
+            System.out.println("Largest is A");
+        }else if(B>=C){
+            System.out.println("Largest is B"); 
+        }else{
+            System.out.println("Largest is C");
+        }
+    }
+}
