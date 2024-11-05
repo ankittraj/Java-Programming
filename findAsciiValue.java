d98b63cade83819e30cb99f64e624c087399e8e8
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2024-11-05 23:46:14 +0530

    Add files via upload

---
+++ findAsciiValue.java
@@ -0,0 +1,17 @@
+//Java program to print ASCII values of a character
+
+import java.util.Scanner;public class findAsciiValue {
+    
+	public static void main(String[] args)
+	{
+		
+		char c='A';	
+		
+		//typecasting from character type to integer type
+		int i = c;
+
+		System.out.println("ASCII value of "+c+" is "+i);
+		
+		
+	}
+}
